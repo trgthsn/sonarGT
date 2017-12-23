ef4ab76fb767f717bbe03d4f63ccd889461b1f12
trgthsn <gthasanturgut@gmail.com>
2017-12-23 18:51:21 +0300

    feature branch code smell

--- src/main/java/sonardenemeleri/sonar/App.java
+++ src/main/java/sonardenemeleri/sonar/App.java
@@ -7,7 +7,12 @@ package sonardenemeleri.sonar;
 public class App 
 {
     public static void main( String[] args )
-    {
+    {final int deneme;
+	int target = -5;
+	int num = 3;
+
+	target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+	target =+ num; // Noncompliant; target = 3
     	
         System.out.println( "Hello World!" );
     }
