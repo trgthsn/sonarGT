3de8bd2837e987416445b5b91eb6f23a5ca0eb30
trgthsn <gthasanturgut@gmail.com>
2017-12-23 18:48:00 +0300

    second commit

--- src/main/java/sonardenemeleri/sonar/App.java
+++ src/main/java/sonardenemeleri/sonar/App.java
@@ -1,19 +1,14 @@
 package sonardenemeleri.sonar;
 
 /**
- * Hello world!
+ * Hello world! deneme
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-    	final int deneme;
-    	int target = -5;
-    	int num = 3;
-
-    	target =- num;  // Noncompliant; target = -3. Is that really what's meant?
-    	target =+ num; // Noncompliant; target = 3
+    	
         System.out.println( "Hello World!" );
     }
 }
